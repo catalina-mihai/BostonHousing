b63ba172d442a0ccada048ab70b7728560d22b73
catalina-mihai <79297440+catalina-mihai@users.noreply.github.com>
2024-10-15 17:38:09 +0200

    Updated Outliers main.py

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ print(data_frame.mean())
 print(data_frame.median())
 print(data_frame.std())
 print(data_frame.isnull().sum())
+
+Q1 = df['medv'].quantile(.25)
+Q3 = df['medv'].quantile(.75)
+IQR = Q3 - Q1
+lower_bound=Q1 - 1.5*IQR
+upper_bound=Q3 + 1.5*IQR
+print(df[df['medv']>upper_bound])
+print(df[df['medv']<lower_bound])
+
